eedd3ca41aa8d0b1477c6ad5ccf010839b95eaaf
Tanay Sinha <tanay.sinha@mail.utoronto.ca>
2023-07-10 13:50:05 +0530

    App.jsx: Use content component

--- src/App.jsx
+++ src/App.jsx
@@ -1,14 +1,13 @@
-import { useState } from 'react'
-import reactLogo from './assets/react.svg'
-import viteLogo from '/vite.svg'
 import './App.css'
 import Navbar from './components/Navbar.jsx'
+import Content from './components/Content.jsx'
+
 
 function App() {
   return (
     <>
       <Navbar/>
-      Hello, World!
+      <Content/>
     </>
   )
 }
