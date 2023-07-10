53850cabd062e357b8b502ddc79262048c954921
Tanay Sinha <tanay.sinha@mail.utoronto.ca>
2023-07-10 15:37:15 +0530

    data.js: Add list of entries for webpage

---
+++ src/data.js
@@ -0,0 +1,37 @@
+let item = [
+    {
+        'key': 1,
+        'img': 'https://images.unsplash.com/photo-1628926867153-b6bad577f000?ixlib=rb-4.0.3&ixid=M3wxMjA3fDB8MHxwaG90by1wYWdlfHx8fGVufDB8fHx8fA%3D%3D&auto=format&fit=crop&w=774&q=80',
+        'url': 'https://www.google.com/maps/place/Shimogo,+Minamiaizu+District,+Fukushima,+Japan/@37.2537878,139.6940951,11z/data=!3m1!4b1!4m6!3m5!1s0x601ffaaa3c93a1a1:0xc0feec6adfe257ca!8m2!3d37.2555639!4d139.8721203!16zL20vMDNrcGp0?entry=ttu',
+        'desc': 'Nestled in the breathtaking countryside of Fukushima, Japan, Shimogo captivates visitors with its serene landscapes, historic charm, and warm hospitality, offering a truly immersive cultural experience.',
+        'start': '12 May, 2020',
+        'end': '23 Sept, 2020',
+        'title': 'Shimogō, Fukushima',
+        'location': 'JAPAN'
+    },
+
+    {
+        'key': 2,
+        'img': 'https://images.unsplash.com/photo-1543731068-7e0f5beff43a?ixlib=rb-4.0.3&ixid=M3wxMjA3fDB8MHxwaG90by1wYWdlfHx8fGVufDB8fHx8fA%3D%3D&auto=format&fit=crop&w=1548&q=80',
+        'url': 'https://www.google.com/maps/place/Olhuveli/@3.8510733,73.446207,16z/data=!4m10!1m2!2m1!1sOlhuveli+islands!3m6!1s0x3b40a09af9106563:0x337858b4df1d86a5!8m2!3d3.8510733!4d73.4552192!15sCg9PbGh1dmVsaSBpc2xhbmRaESIPb2xodXZlbGkgaXNsYW5kkgEGaXNsYW5k4AEA!16s%2Fg%2F1221h81z?entry=ttu',
+        'desc': 'Set amidst the turquoise waters of the Maldives, Olhuveli Island beckons with its pristine beaches, vibrant coral reefs, and luxurious overwater bungalows, inviting travelers to indulge in a tropical paradise like no other.',
+        'start': '11 Aug, 2021',
+        'end': '31 Aug, 2021',
+        'title': 'Olhuveli Island',
+        'location': 'MALDIVES'
+    },    
+
+    {
+        'key': 3,
+        'img': 'https://images.unsplash.com/photo-1547981609-4b6bfe67ca0b?ixlib=rb-4.0.3&ixid=M3wxMjA3fDB8MHxwaG90by1wYWdlfHx8fGVufDB8fHx8fA%3D%3D&auto=format&fit=crop&w=1740&q=80',
+        'url': 'https://www.google.com/maps/place/Forbidden+City/@39.9165742,116.3945834,17z/data=!4m10!1m2!2m1!1sforbidden+city+china!3m6!1s0x35f052e94515d43d:0x674e2bd4dd3079f!8m2!3d39.9168038!4d116.3971621!15sChRmb3JiaWRkZW4gY2l0eSBjaGluYVoWIhRmb3JiaWRkZW4gY2l0eSBjaGluYZIBEnRvdXJpc3RfYXR0cmFjdGlvbuABAA!16zL20vMGowYjI?entry=ttu',
+        'desc': "A symbol of China's rich imperial history, the Forbidden City in Beijing stands as a majestic architectural masterpiece, captivating travelers with its grandeur, intricate details, and profound cultural significance, making it a must-visit destination for history enthusiasts and cultural explorers alike.",
+        'start': '5 June, 2022',
+        'end': '14 Jul, 2022',
+        'title': 'Forbidden City',
+        'location': 'CHINA'
+    }    
+    
+].reverse()
+
+export default item
