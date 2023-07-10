c806163853f5c4102597689d270ac7eed4835add
Tanay Sinha <tanay.sinha@mail.utoronto.ca>
2023-07-10 16:18:27 +0530

    Footer: Add and style footer

---
+++ src/components/Footer.jsx
@@ -0,0 +1,11 @@
+import './Navbar.css'
+import './Footer.css'
+
+export default function Footer(props)
+{
+    return (
+        <div className='footer'>
+            <p className='footer--text'> Made by Tanay Sinha </p>
+        </div>
+    )
+}
